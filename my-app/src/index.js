fb9420196d266b4109d95aafcbcde7d79014a74a
MarianaMolina <00046416@uca.edu.sv>
2019-11-04 23:40:07 -0600

    React

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Formulario extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isGoing: true,
+        numberOfGuests: 2
+      };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleInputChange = this.handleInputChange.bind(this);
+    }
+  
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
+    }
+
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit} class="jumbotron">
+            
+            <h1>Registro de laboratorio</h1>
+            <div class="form-group">
+            <label class="col-sm-2 col-form-label">Ingrese el carnet: </label>
+            <input class="form-control" type="text" ></input>
+            </div>
+
+            <div class="form-group">
+                <label for="schedule">Seleccione el horario:</label>
+                <select name="schedule" class="form-control" id="schedule_field">
+                    <option>Lunes de 9:00 a 11.00</option>
+                    <option>Martes de 13:30 a 15:30</option>
+                    <option>Miércoles de 9:00 a 11.00</option>
+                    <option>Jueves de 13:30 a 15:30</option>
+                    <option>Viernes de 9:00 a 11.00</option>
+                    <option>Viernes de 15:30 a 17:30</option>
+                </select>
+            </div>  
+            
+            <div class="form-group">
+                <div class="custom-control custom-switch">
+                    <input type="checkbox" class="custom-control-input" id="late_switch"></input>
+                    <label class="custom-control-label" for="late_switch">Llegó tarde? </label>
+                </div>
+            </div>
+            
+        
+            <div class="form-group">
+                <button type="button" class="btn btn-danger" id="submit_btn"> Ingresar</button>     
+            </div>
+    
+        </form>
+        );
+    }
+}
+
+//main
+ReactDOM.render(
+    <Formulario />,
+    document.getElementById('root')
+  );
+
+
+ 
